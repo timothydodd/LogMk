018a520520ef819689d19605896d0935b52e8a5e
Timothy Dodd <tdodd@viewfi.com>
2024-08-02 10:51:21 -0400

    Fixed Logline again

--- src/LogMkCommon/Logline.cs
+++ src/LogMkCommon/Logline.cs
@@ -6,7 +6,7 @@ public class LogLine
     public required string PodName { get; set; }
     public required string Line { get; set; }
     public required LogLevel LogLevel { get; set; }
-    public required DateTime TimeStamp { get; set; }
+    public required DateTimeOffset TimeStamp { get; set; }
 }
 public enum LogLevel
 {
